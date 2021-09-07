a3854334628568e7f302c86c2fa876ca9814743c
GitHub Action <action@github.com>
2021-09-07 20:19:03 +0000

    Template cleanup

---
+++ src/main/kotlin/com/github/casheridan/doomslayerprogressbar/services/MyApplicationService.kt
@@ -0,0 +1,10 @@
+package com.github.casheridan.doomslayerprogressbar.services
+
+import com.github.casheridan.doomslayerprogressbar.MyBundle
+
+class MyApplicationService {
+
+    init {
+        println(MyBundle.message("applicationService"))
+    }
+}
